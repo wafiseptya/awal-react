aca31606f0f557871440c56eeb72b237ef46a3bb
Wafi <septya.wafi@gmail.com>
2019-03-18 13:14:28 +0700

    Updates

---
+++ precache-manifest.7b059e25caa9fded6d07561c919ebb57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/awal-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0ddb13cc11bfb1a0a0d6",
+    "url": "/awal-react/static/js/runtime~main.b85d8e09.js"
+  },
+  {
+    "revision": "c2deb37d476dcb0e4fbd",
+    "url": "/awal-react/static/js/main.48460985.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/awal-react/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "c2deb37d476dcb0e4fbd",
+    "url": "/awal-react/static/css/main.b235118a.chunk.css"
+  },
+  {
+    "revision": "a91fd39635997cc1dde78beb0c82c80c",
+    "url": "/awal-react/index.html"
+  }
+];
